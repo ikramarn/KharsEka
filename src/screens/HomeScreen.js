8c6a115f03650f7f2a7e20ddc1434a362e9d29e0
KharsEka Dev <dev@example.local>
2025-12-26 15:02:27 +0000

    feat: update home screen title

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,7 +25,7 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Khars eka ao Bel try ka</Text>
+      <Text style={styles.title}>Raza che drla e Khars ku</Text>
       <Text style={styles.header}>Browse Listings</Text>
       <View style={{ marginBottom: 12 }}>
         <Button title="Create Listing" onPress={() => navigation.navigate('Create')} />
